6e3aecb15615aca4797d9d95c8bfd29df1039a6e
nicohnavarro <nicohnavarro@gmail.com>
2021-02-04 04:54:05 +0000

    :fire: working with chakra-ui

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import {Formik, Form} from 'formik';
+import {FormErrorMessage, FormLabel, Input} from '@chakra-ui/react';
+import Wrapper from './Wrapper';
 
 interface registerProps{
 
 }
 
 const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register Page</div>;
-}
+  return (
+  <Wrapper variant="small">
+    <Formik
+    initialValues = {{username:"", password:""}}
+    onSubmit={(values) => {
+      console.log(values);
+    }}
+    >
+    {({values, handleChange})=> (
+      <Form>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input 
+          value={values.username}
+          onChange={handleChange}
+          id="username" 
+          placeholder="username"/>
+        <FormErrorMessage></FormErrorMessage>
+      </Form>
+    )}
+    </Formik>
+  </Wrapper>
+  );
+};
 
 export default Register;
