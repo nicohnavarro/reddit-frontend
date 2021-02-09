13ee13d92fe1c7b1bd87e358944aa8ea6e6914a7
nicohnavarro <nicohnavarro@gmail.com>
2021-02-09 23:25:51 +0000

    :fire: add input

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Formik, Form} from 'formik';
-import {FormErrorMessage, FormLabel, Input} from '@chakra-ui/react';
 import Wrapper from './Wrapper';
+import {InputField} from '../components/InputField';
+import {Button, Box} from '@chakra-ui/react';
 
 interface registerProps{
 
@@ -16,15 +17,22 @@ const Register: React.FC<registerProps> = ({}) => {
       console.log(values);
     }}
     >
-    {({values, handleChange})=> (
+    {({isSubmitting})=> (
       <Form>
-        <FormLabel htmlFor="username">Username</FormLabel>
-        <Input 
-          value={values.username}
-          onChange={handleChange}
-          id="username" 
-          placeholder="username"/>
-        <FormErrorMessage></FormErrorMessage>
+        <Box mt={4}>
+          <InputField name='username' placeholder='username' label='Username'/>
+        </Box>
+        <Box mt={4}>
+          <InputField name='password' placeholder='password' label='Password' type='password'/>
+        </Box>
+
+        <Button 
+        mt={4}
+        type="submit"
+        isLoading={isSubmitting}
+        variantColor="teal">
+        Register  
+      </Button>
       </Form>
     )}
     </Formik>
