99efb76358b72ab5b8d25dbd91e91dcccfc6f47d
nicohnavarro <nicohnavarro@gmail.com>
2021-02-10 04:14:02 +0000

    :lock: some cors problems

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -32,9 +32,9 @@ const Register: React.FC<registerProps> = ({}) => {
   <Wrapper variant="small">
     <Formik
     initialValues = {{username:"", password:""}}
-    onSubmit={(values) => {
+    onSubmit={async (values) => {
       console.log(values);
-      return register(values);
+      const response = await register(values);
     }}
     >
     {({isSubmitting})=> (
