c02d1849ce00ba8cf7468010a1098806fc64cd12
nicohnavarro <nicohnavarro@gmail.com>
2021-02-10 03:04:39 +0000

    :fire: add some code

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,34 +3,54 @@ import {Formik, Form} from 'formik';
 import Wrapper from './Wrapper';
 import {InputField} from '../components/InputField';
 import {Button, Box} from '@chakra-ui/react';
+import {useMutation} from 'urql';
 
 interface registerProps{
 
 }
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password:String!){
+  register(options:{ username: $username, password:$password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+
+`
+
 const Register: React.FC<registerProps> = ({}) => {
+
+  const [,register] = useMutation(REGISTER_MUT);
   return (
   <Wrapper variant="small">
     <Formik
     initialValues = {{username:"", password:""}}
     onSubmit={(values) => {
       console.log(values);
+      return register(values);
     }}
     >
     {({isSubmitting})=> (
       <Form>
         <Box mt={4}>
-          <InputField name='username' placeholder='username' label='Username'/>
+          <InputField name='username' label='Username'/>
         </Box>
         <Box mt={4}>
-          <InputField name='password' placeholder='password' label='Password' type='password'/>
+          <InputField name='password' label='Password' type='password'/>
         </Box>
 
         <Button 
         mt={4}
         type="submit"
         isLoading={isSubmitting}
-        variantColor="teal">
+        variantcolor="teal">
         Register  
       </Button>
       </Form>
